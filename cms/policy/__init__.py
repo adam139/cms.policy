ccc5410e8570a50d6c3240637b3deffabd9fbd86
adam139 <yuejun.tang@gmail.com>
2019-11-14 20:12:02 +0800

    add browser sub-package for customizing main_template

--- cms/policy/__init__.py
+++ cms/policy/__init__.py
@@ -1,6 +1,8 @@
-# -*- extra stuff goes here -*-
+#-*- coding: UTF-8 -*-
+from zope.i18nmessageid import MessageFactory
 
 fmt = "%Y-%m-%d %H:%M:%S"
+_ = MessageFactory('cms.policy')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
